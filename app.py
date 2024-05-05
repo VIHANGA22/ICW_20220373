a3e34bcb890ac51bca981905f7563cd182dd8f16
VIHANGA22 <165758303+VIHANGA22@users.noreply.github.com>
2024-05-05 22:14:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,15 @@ st.markdown('<div class="center subheader"><h3>Analyze Your Sales Data<h3></div>
 
 # Read the data
 sales_data = pd.read_excel("cleaned_dataset.xlsx", engine='openpyxl')
+# Define chart configurations
+charts_info = [
+    {"type": "box", "x": "Sub-Category", "y": "Quantity", "title": "Box Plot", "color": "Sub-Category"},
+    {"type": "bar", "x": "Ship Mode", "y": "Shipping Cost", "title": "Bar Chart", "color": "Ship Mode"},
+    {"type": "pie", "names": "Order Priority", "title": "Donut Chart", "hole": 0.5},
+    {"type": "histogram", "x": "Sales", "title": "Histogram"},
+    {"type": "area", "x": "Market", "y": "Profit", "title": "Area Chart"},
+    {"type": "density_heatmap", "x": "Country", "y": "Sales", "title": "Heatmap of Top 10 Countries in Sales", "color_scale": "reds"}
+]
 
 
 
